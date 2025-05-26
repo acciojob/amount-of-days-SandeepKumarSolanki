a357f0e553d30530b95bbd11cd3229180166c01e
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-26 18:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+// let year = Number(prompt("Enter random years between 1 - 9999"));
+
+function daysOfAYear(year){
+	if(year <= 1 && years >= 9999){
+		return `Input valid year`
+	}
+	if(year % 4 == 0 ){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+module.exports daysOfAYear
