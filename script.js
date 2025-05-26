b8db84ed26102acd6ae5b6335c0ada7bb3707464
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-26 18:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  if (year < 1 || year > 9999) {
+    throw new Error("Year must be between 1 and 9999.");
+  }
 
-// let year = Number(prompt("Enter random years between 1 - 9999"));
+  // Leap year logic:
+  // - divisible by 4
+  // - not divisible by 100 unless also divisible by 400
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
-function daysOfAYear(year){
-	if(year <= 1 && years >= 9999){
-		return `Input valid year`
-	}
-	if(year % 4 == 0 ){
-		return 366;
-	}else{
-		return 365;
-	}
+  return isLeapYear ? 366 : 365;
 }
-
-module.exports daysOfAYear
